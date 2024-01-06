6e0703405531e2448e6efb0a3d94275646b6ede3
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2024-01-06 13:54:15 +0400

    responsive menu updated

--- src/app/components/layout/header/contact/page.tsx
+++ src/app/components/layout/header/contact/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
-export default function Search(){
+export default function Contact(){
  
     return (
-        <div className="">
+        <div className="hidden md:show">
             <button className="bg-transparent hover:bg-[#531D60] text-[#531D60] hover:text-white py-2 px-4 border border-[#531D60] hover:border-transparent rounded-full">
                 <Link href=''> Contact Us</Link>
             </button>
--- src/app/components/layout/header/navbar/page.tsx
+++ src/app/components/layout/header/navbar/page.tsx
@@ -12,63 +12,86 @@ export default function Navbar () {
         setisClick(!isClick)
     }
   return (
-    <div>
-    <div>
-        <ul className='md:flex justify-around text-md'>
-        <li className='md:inline-block mx-5 border-b-2 border-b-[#531D60]'>
-            <Link href="/">Home</Link>
-        </li>
-        <li className='md:inline-block mx-5 hover:border-b-2 hover:border-b-[#531D60]'>
-            <Link href="/agency">Web Agency</Link>
-        </li>
-        <li className='md:inline-block mx-5 hover:border-b-2 hover:border-b-[#531D60]'>
-            <Link href="/services">Services</Link>
-        </li>
-        <li className='md:inline-block mx-5 hover:border-b-2 hover:border-b-[#531D60]'>
-            <Link href="/about">About us</Link>
-        </li>
-        </ul>
-    </div>
-    <div className="md:hidden flex items-center">
-        <button className="inline-flex items-center justify-center p-2 focus:outline-none focus:ring-2 focus:ring-insert focuse:ring-white"
-        onClick={toggleNavbar}
-        >
-            {isClick ? (
-                <svg 
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-                >
-                </path>
-                </svg>
-            ): (
-                <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
+    <nav>
+        <div>
+            <ul className='hidden md:flex justify-around text-md'>
+            <li className='md:inline-block mx-5 border-b-2 border-b-[#531D60]'>
+                <Link href="/">Home</Link>
+            </li>
+            <li className='md:inline-block mx-5 hover:border-b-2 hover:border-b-[#531D60]'>
+                <Link href="/agency">Web Agency</Link>
+            </li>
+            <li className='md:inline-block mx-5 hover:border-b-2 hover:border-b-[#531D60]'>
+                <Link href="/services">Services</Link>
+            </li>
+            <li className='md:inline-block mx-5 hover:border-b-2 hover:border-b-[#531D60]'>
+                <Link href="/about">About us</Link>
+            </li>
+            </ul>
+        </div>
+        <div className="md:hidden flex items-center">
+            <button className="inline-flex items-center justify-center p-2 focus:outline-none focus:ring-2 focus:ring-insert focuse:ring-white"
+            onClick={toggleNavbar}
+            >
+                {isClick ? (
+                    <svg 
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                    >
+                    </path>
+                    </svg>
+                ): (
+                    <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
 
-                />
-                </svg>
-            )
-            }
-        </button>
+                    />
+                    </svg>
+                )
+                }
+            </button>
 
-    </div>
-    </div>
+        </div>
+        {isClick && (
+            <div className="md:hidden">
+                <div className="px-2 pt-2 pb-3 space-y- sm:px-32">
+                <ul className="md:hidden md:flex justify-end text-md">
+                    <li className='md:inline-block block mx-5 border-b-2 py-4 border-b-[#531D60]'>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className='md:inline-block block mx-5 py-4 hover:border-b-2 hover:border-b-[#531D60]'>
+                        <Link href="/agency">Web Agency</Link>
+                    </li>
+                    <li className='md:inline-block block mx-5 py-4 hover:border-b-2 hover:border-b-[#531D60]'>
+                        <Link href="/services">Services</Link>
+                    </li>
+                    <li className='md:inline-block block mx-5 py-4 hover:border-b-2 hover:border-b-[#531D60]'>
+                        <Link href="/about">About us</Link>
+                    </li>
+                </ul>
+                </div>
+
+            </div>
+        )
+        }
+    </nav>
+
   )
 }
 
