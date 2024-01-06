b39c6852c9cdc5c5564b5543a20cc9cd8c2bad3a
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2024-01-06 15:50:41 +0400

    responsive fixed

--- src/app/components/layout/header/hero/page.tsx
+++ src/app/components/layout/header/hero/page.tsx
@@ -3,22 +3,22 @@ import Image from "next/image"
 
 export default function Hero () {
   return (
-    <div className="md:flex md:justify-around">
-        <div className="px-10 py-20">
-            <h1 className="text-2xl md:text-6xl">
+    <div className="flex md:justify-around">
+        <div className="px-10 py:10 md:py-20">
+            <h1 className="text-4xl md:text-6xl">
                 Web Design & Development Company
             </h1>
-            <p className="text-4xl py-10">
+            <p className="text-2xl md:text-4xl py-10">
             NisNass, a distinguished web design and development company based in Dubai.
             </p>
-            <div className="md:flex">
+            <div className="flex">
                 <div>
-                    <button className="bg-[#531D60] text-white px-4 py-1 md:py-3 md:px-8 border border-[#531D60] hover:border-transparent rounded-full ">
+                    <button className="bg-[#531D60] text-white py-3 px-8 border border-[#531D60] hover:border-transparent rounded-full ">
                         <Link href='/'> Read More</Link>
                     </button>
                 </div>
                 <div>
-                <button className="bg-transparent hover:bg-[#531D60] md:py-3 md:px-8  text-[#531D60] hover:text-white py-2 px-4 border border-[#531D60] hover:border-transparent rounded-full ml-6">
+                <button className="bg-transparent hover:bg-[#531D60] py-3 px-8 text-[#531D60] hover:text-white border border-[#531D60] hover:border-transparent rounded-full ml-6">
                     <Link href='/'> Contact Us</Link>
                 </button>
                 </div>
