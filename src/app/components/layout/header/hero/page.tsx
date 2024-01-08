d9431c849f9a0d32d504879271d1408449f0e731
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2024-01-08 23:36:12 +0400

    update

--- src/app/components/layout/header/hero/page.tsx
+++ src/app/components/layout/header/hero/page.tsx
@@ -1,32 +1,32 @@
-import Link from "next/link"
-import Image from "next/image"
+// import Link from "next/link"
+// import Image from "next/image"
 
-export default function Hero () {
-  return (
-    <div className="md:flex md:justify-around">
-        <div className="px-10 py:10 md:py-20">
-            <h1 className="text-4xl md:text-6xl">
-                Web Design & Development Company
-            </h1>
-            <p className="text-2xl md:text-4xl py-10">
-            NisNass, a distinguished web design and development company based in Dubai.
-            </p>
-            <div className="md:flex">
-                <div>
-                    <button className="bg-[#531D60] text-white py-3 px-8 border border-[#531D60] hover:border-transparent rounded-full ">
-                        <Link href='/'> Read More</Link>
-                    </button>
-                </div>
-                <div className="mt-4 md:mt-0">
-                <button className="bg-transparent hover:bg-[#531D60] py-3 px-8 text-[#531D60] hover:text-white border border-[#531D60] hover:border-transparent rounded-full md:ml-6">
-                    <Link href='/'> Contact Us</Link>
-                </button>
-                </div>
-            </div>
-        </div> 
-        <div>
-            <Image src="/galax2.png" className="" width={600} height={400} alt="hero" />
-        </div>
-    </div>
-  )
-}
+// export default function Hero () {
+//   return (
+//     <div className="md:flex md:justify-around">
+//         <div className="px-10 py:10 md:py-20">
+//             <h1 className="text-4xl md:text-6xl">
+//                 Web Design & Development Company
+//             </h1>
+//             <p className="text-2xl md:text-4xl py-10">
+//             NisNass, a distinguished web design and development company based in Dubai.
+//             </p>
+//             <div className="md:flex">
+//                 <div>
+//                     <button className="bg-[#531D60] text-white py-3 px-8 border border-[#531D60] hover:border-transparent rounded-full ">
+//                         <Link href='/'> Read More</Link>
+//                     </button>
+//                 </div>
+//                 <div className="mt-4 md:mt-0">
+//                 <button className="bg-transparent hover:bg-[#531D60] py-3 px-8 text-[#531D60] hover:text-white border border-[#531D60] hover:border-transparent rounded-full md:ml-6">
+//                     <Link href='/'> Contact Us</Link>
+//                 </button>
+//                 </div>
+//             </div>
+//         </div> 
+//         <div>
+//             <Image src="/galax2.png" className="" width={600} height={400} alt="hero" />
+//         </div>
+//     </div>
+//   )
+// }
